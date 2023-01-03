35ab29459509ab11b15abe408d9e86eaab5bb68f
walacefex <walacefex01@gmail.com>
2023-01-03 19:06:21 -0300

    writing tests to fail and validate if the values ​​are numbers

--- projeto1/calculator.spec.js
+++ projeto1/calculator.spec.js
@@ -5,3 +5,25 @@ it('sum 2 and 2 and the result must be 4', () => {
   /*Using the To Be method to check if the value is as expected.*/
   expect(sum(2, 2)).toBe(4); 
 });
+
+it('should sum 2 and 2 even if one of them is a string and the result must be 4', () => {
+  expect(sum('2', '2')).toBe(4); 
+});
+
+it('should throw an error if what is provided to the method cannot be summed', () => {
+  expect( () => {
+    sum('', '2');
+  }).toThrowError();
+
+  expect( () => {
+    sum([2 ,2]);
+  }).toThrowError();
+
+  expect( () => {
+    sum({});
+  }).toThrowError();
+
+  expect( () => {
+    sum();
+  }).toThrowError();
+});
