e24e4b775d776bf2b850542b6809c6cb6f2e57bf
walacefex <walacefex01@gmail.com>
2023-01-11 17:34:56 -0300

    add babel and improving the code, adding ES6 syntax when importing functions

--- projeto1/src/lib/calculator.spec.js
+++ projeto1/src/lib/calculator.spec.js
@@ -1,29 +1,29 @@
-const {sum} = require('./calculator')
+import { sum } from './calculator';
 it('sum 2 and 2 and the result must be 4', () => {
   /*Using not to negate the value.*/
-  //expect(sum(2, 2)).not.toBe(4) 
+  //expect(sum(2, 2)).not.toBe(4)
   /*Using the To Be method to check if the value is as expected.*/
-  expect(sum(2, 2)).toBe(4); 
+  expect(sum(2, 2)).toBe(4);
 });
 
 it('should sum 2 and 2 even if one of them is a string and the result must be 4', () => {
-  expect(sum('2', '2')).toBe(4); 
+  expect(sum('2', '2')).toBe(4);
 });
 
 it('should throw an error if what is provided to the method cannot be summed', () => {
-  expect( () => {
+  expect(() => {
     sum('', '2');
   }).toThrowError();
 
-  expect( () => {
-    sum([2 ,2]);
+  expect(() => {
+    sum([2, 2]);
   }).toThrowError();
 
-  expect( () => {
+  expect(() => {
     sum({});
   }).toThrowError();
 
-  expect( () => {
+  expect(() => {
     sum();
   }).toThrowError();
 });
